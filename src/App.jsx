ab9b8c94e506b150c0e74e6dc11445c7e9f0ee6c
mrseeker53 <mi.nayeem30@gmail.com>
2024-06-10 16:21:40 +0600

    Setup the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Calendar from "./components/Calendar/Calendar";
+
+function App() {
+
+  return (
+    <div className="container">
+      <Calendar></Calendar>
+    </div>
+  )
+}
+
+export default App;
