a1b59b9c877223eb925d90703d6497ac2128122c
mrseeker53 <mi.nayeem30@gmail.com>
2024-06-10 17:37:48 +0600

    feat: Add functionality to the Calendar component

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { addMonths, format, subMonths } from "date-fns";
+import {
+	addMonths,
+	eachDayOfInterval,
+	endOfMonth,
+	format,
+	startOfMonth,
+	subMonths,
+} from "date-fns";
 import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 
 const Calendar = () => {
@@ -16,18 +23,35 @@ const Calendar = () => {
 		setCurrentMonth(addMonths(currentMonth, 1));
 	};
 
+	// Function to generate array of days for current month
+	const generateCalendarDays = () => {
+		const start = startOfMonth(currentMonth);
+		const end = endOfMonth(currentMonth);
+		return eachDayOfInterval({ start, end });
+	};
+
+	const calendarDays = generateCalendarDays();
+
 	return (
 		<div className="calendar">
+			{/* Calendar Header - Month & Year with navigation buttons */}
 			<div className="header">
 				<button onClick={prevMonth}>
 					<RiArrowLeftSLine />
 				</button>
-                <h2>{format(currentMonth, "MMMM yyyy")}</h2>
+				<h2>{format(currentMonth, "MMMM yyyy")}</h2>
 				<button onClick={nextMonth}>
 					<RiArrowRightSLine />
 				</button>
 			</div>
-            {/* Calendar Days */}
+			{/* Calendar Days */}
+			<div className="days">
+				{calendarDays.map((day) => (
+					<div className="day" key={day}>
+						{format(day, "d")}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
