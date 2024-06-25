6191550d48a9c3f5becfb7f631907b88971576f3
mrseeker53 <mi.nayeem30@gmail.com>
2024-06-25 15:44:05 +0600

    update style for Calendar Header

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { addMonths, subMonths } from "date-fns";
 import CalendarHeader from "./CalendarHeader";
-import CalendarWeek from "./CalendarWeek";
-import CalendarDays from "./CalendarDays";
-import TaskModal from "../Task/TaskModal";
+// import CalendarWeek from "./CalendarWeek";
+// import CalendarDays from "./CalendarDays";
 import TasksDisplay from "../Task/TasksDisplay";
 import { useSelector } from "react-redux";
 
@@ -27,28 +26,15 @@ const Calendar = () => {
 	};
 
 	return (
-		<div className="flex bg-white rounded-xl w-full my-6">
-			<div className="bg-white rounded-xl shadow-xl px-4 py-6">
+		<div className="bg-white rounded-xl w-full my-6">
+			<div className="rounded-lg px-4 py-6">
 				<CalendarHeader
 					currentMonth={currentMonth}
 					prevMonth={prevMonth}
 					nextMonth={nextMonth}
 				/>
-				<CalendarWeek currentMonth={currentMonth} />
-				<CalendarDays
-					currentMonth={currentMonth}
-					selectedDate={selectedDate}
-					onDateClick={(day) => setSelectedDate(day)}
-					tasks={tasks}
-				/>
-				{showModal && (
-					<TaskModal
-						selectedDate={selectedDate}
-						onClose={() => setShowModal(false)}
-					/>
-				)}
 			</div>
-			<div className="bg-white p-2 shadow-2xl rounded-xl ml-12 w-3/4">
+			<div className="bg-white p-4 drop-shadow-2xl rounded-xl">
 				<TasksDisplay
 					currentMonth={currentMonth}
 					onAddTasksClick={openModal}
--- src/components/Calendar/CalendarHeader.jsx
+++ src/components/Calendar/CalendarHeader.jsx
@@ -5,14 +5,15 @@ import { format } from "date-fns";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const CalendarHeader = ({ currentMonth, prevMonth, nextMonth }) => {
-	
-
 	return (
-		<div className="flex justify-between items-center mb-4 px-2">
-			<span className="text-lg font-semibold text-stone-900 pl-2">
-				{format(currentMonth, "MMMM yyyy")}
-			</span>
-			<div className="flex items-center space-x-4">
+		<div className="flex items-center mb-4 px-2">
+			<div className="flex items-center justify-between">
+				<h3 className="text-gray-800 text-3xl font-semibold cursor-pointer drop-shadow-xl pr-36">Calendar</h3>
+				<span className="text-xl font-semibold text-gray-800 pt-1">
+					{format(currentMonth, "MMMM yyyy")}
+				</span>
+			</div>
+			<div className="flex items-center space-x-4 pl-6 pt-1">
 				<button
 					onClick={prevMonth}
 					className="p-1.5 bg-white text-gray-700 hover:bg-slate-100 border-none focus:outline-none focus:bg-slate-200 rounded-full"
